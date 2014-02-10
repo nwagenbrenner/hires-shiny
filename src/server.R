d701064e8dfac29685752bf514dba80e10cca236
Natalie Wagenbrenner <natalie@forest.moscowfsl.wsu.edu>
2014-02-09 16:50:51 -0800

    update for using windtools

--- src/server.R
+++ src/server.R
@@ -1,112 +1,79 @@
 library(shiny)
 #library(windtools)
 
-library('RSQLite')
+#set default db as src
+#db<-'/home/natalie/test/src.sqlite'
 
-db<-'/FVS/shiny-server/shinyWindTools/src.sqlite'
-start_time = '2011-08-15 06:00:00'
-end_time = '2011-08-20 06:00:00'
-
-con <- dbConnect('SQLite', dbname = db)
-sql <- paste0("SELECT * FROM mean_flow_obs ",
-            "WHERE Date_time BETWEEN '", start_time, "' ", "AND '", end_time, "' ",
-            "AND Quality='OK'", collapse="")
-res <- dbSendQuery(con, statement = sql)
-src <- fetch(res, n = -1) #fetch all data
-dbClearResult(res)
-dbDisconnect(con)
-
-db<-'/FVS/shiny-server/shinyWindTools/bsb.sqlite'
-start_time = '2010-08-15 06:00:00'
-end_time = '2010-08-20 06:00:00'
-
-con <- dbConnect('SQLite', dbname = db)
-sql <- paste0("SELECT * FROM mean_flow_obs ",
-            "WHERE Date_time BETWEEN '", start_time, "' ", "AND '", end_time, "' ",
-            "AND Quality='OK'", collapse="")
-res <- dbSendQuery(con, statement = sql)
-bsb <- fetch(res, n = -1) #fetch all data
-dbClearResult(res)
-dbDisconnect(con)
-colnames(bsb)<-c("plot_id", "date_time", "wind_speed", "wind_gust", "wind_dir", "qualtiy", "sensor_qual")
-
-# Define server logic
+# Define server logic required to plot various variables against mpg
 shinyServer(function(input, output) {
   
   # Compute the forumla text in a reactive expression since it is 
   # shared by the output$caption and output$mpgPlot expressions
+  
   formulaText <- reactive({
-    paste("Wind Speed: ", input$variable)
+    paste("Wind Speed: ")
+    #paste("Wind Speed: ", input$variable)
+  })
+  formulaText1 <- reactive({
+    paste("Site Location: ", input$site)
   })
 
   # Return the formula text for printing as a caption
+  output$caption1 <- renderText({
+    formulaText1()
+  })
+  
   output$caption <- renderText({
     formulaText()
   })
 
-  output$selectUI <- renderUI({ 
-    if(input$site == 'SRC'){
-      selectInput('variable', 'Choose a sensor:', unique(src$plot_id))
-    }
-    else if(input$site == 'BSB'){
-      selectInput('variable', 'Choose a Sensor:', unique(bsb$plot_id))
-    }
-  })
-
   # Generate a plot of the requested sensor speed 
-  output$speedPlot <- renderPlot({
-     library(ggplot2)
-     if(input$site == 'SRC'){
-       s<-subset(src, subset=(plot_id==input$variable))
-     }
-     else if(input$site == 'BSB'){
-       s<-subset(bsb, subset=(plot_id==input$variable))
-     }
-     s$date_time2<-as.POSIXct(strptime(s[,"date_time"], '%Y-%m-%d %H:%M:%S'))
-     
-     p<-ggplot(s, aes(x=date_time2, y=wind_speed)) + 
-        geom_point(shape=19, size=1.5, color='blue') + 
-        theme_bw() +
-        xlab("Time") + 
-        ylab("Observed Speed (m/s)") +
-        ggtitle(input$variable)
+  #output$speedPlot <- renderPlot({
+  #  s<-subset(d, subset=(plot==input$variable))
+  #  p<-plotSensorSpeed(d, input$variable)
+  #  print(p)
+  #})
   
-     p<-p + scale_x_datetime(breaks=c(min(s$date_time2),
+  output$speedPlot <- renderPlot({
+    #if(input$site == 'SRC'){
+      #db<-'/home/natalie/test/src.sqlite'
+    #}
+    #else if(input$site == 'BSB'){
+      #db<-'/home/natalie/test/bsb.sqlite'
+    #}
+   library(ggplot2)
+   db<-'/home/natalie/test/src.sqlite'
+   start_time = '2011-08-15 06:00:00'
+   end_time = '2011-08-20 06:00:00'
+   data<-dbFetchAll(db, start_time, end_time)
+   
+   s<-subset(data, subset=(plot_id=='K2'))
+   
+   s$date_time2<-as.POSIXct(strptime(s[,"date_time"], '%Y-%m-%d %H:%M:%S'))
+
+   p<-ggplot(s, aes(x=date_time2, y=wind_speed)) + 
+      geom_point(shape=19, size=1.5, color='blue') + 
+      theme_bw() +
+      xlab("Time") + 
+      ylab("Observed Speed (m/s)")
+  
+   p<-p + scale_x_datetime(breaks=c(min(s$date_time2),
                        (max(s$date_time2) - min(s$date_time2))/4 + min(s$date_time2),
                        (max(s$date_time2) - min(s$date_time2))/4*2 + min(s$date_time2),
                        (max(s$date_time2) - min(s$date_time2))/4*3 + min(s$date_time2),
                        max(s$date_time2)))
 
-     p <- p + theme(axis.text.x = element_text(angle = 45))
-     p <- p + theme(axis.text.x = element_text(vjust = 0.5))
+   p <- p + theme(axis.text.x = element_text(angle = 45))
+   p <- p + theme(axis.text.x = element_text(vjust = 0.5))
 
-     p <- p + theme(axis.text = element_text(size = 14))
-     p <- p + theme(axis.title = element_text(size = 14))
+   p <- p + theme(axis.text = element_text(size = 14))
+   p <- p + theme(axis.title = element_text(size = 14))
    
-     print(p)
+   print(p)
+   
   })
+  
 
-  # Generate a summary of the data
-  output$summary <- renderPrint({
-    if(input$site == 'SRC'){
-        s<-subset(src, subset=(plot_id==input$variable))
-    }
-    else if(input$site == 'BSB'){
-        s<-subset(bsb, subset=(plot_id==input$variable))
-    }
-    summary(s)
-  })
-
-  # Generate an HTML table view of the data
-  output$table <- renderTable({
-    if(input$site == 'SRC'){
-        s<-subset(src, subset=(plot_id==input$variable))
-    }
-    else if(input$site == 'BSB'){
-        s<-subset(bsb, subset=(plot_id==input$variable))
-    }
-    data.frame(x=s)
-  })
 })
 
 
--- src/ui.R
+++ src/ui.R
@@ -1,34 +1,52 @@
 library(shiny)
+#library(windtools)
 
-# Define UI for high-res wind application
+#set default db as src
+library('RSQLite')
+db<-'/home/natalie/test/src.sqlite'
+start_time = '2011-08-15 06:00:00'
+end_time = '2011-08-20 06:00:00'
+
+con <- dbConnect('SQLite', dbname = db)
+    
+sql <- paste0("SELECT * FROM mean_flow_obs ", 
+            "WHERE Date_time BETWEEN '", start_time, "' ", "AND '", end_time, "' ",
+            "AND Quality='OK'", collapse="")
+            
+res <- dbSendQuery(con, statement = sql)
+data <- fetch(res, n = -1) #fetch all data
+dbClearResult(res)
+dbDisconnect(con)
+
+#data<-dbFetchAll(db, start_time, end_time)
+
+# Define UI for miles per gallon application
 shinyUI(pageWithSidebar(
 
   # Application title
-  headerPanel("High-resolution Wind Data Access"),
+  headerPanel("High-resolution Wind Data Access for Big Southern Butte and Salmon River Canyon"),
 
   sidebarPanel(
-    selectInput("site", "Choose a site:",
+    selectInput("site", "Site:",
                 list("BSB" = "BSB", 
                      "SRC" = "SRC")),
     
-    htmlOutput("selectUI"),
+    selectInput('variable', 'Sensor', unique(data$plot_id)),
 
-    #selectInput('variable', 'Sensor', unique(data$plot_id)),
+                     
+    #selectInput("variable", "Sensor:",
+    #            list("R2" = "R2", 
+    #                 "TSW8" = "TSW8", 
+    #                 "R5" = "R5")),
 
-    checkboxInput("outliers", "Show suspect data (not active)", FALSE),
-    checkboxInput("vectorPlot", "Create a vector plot (not active)", FALSE),
-    downloadButton('downloadData', 'Download')
+    checkboxInput("outliers", "Show suspect data", FALSE)
   ),
 
   # Show the caption and plot of the requested variable against speed
   mainPanel(
-    tabsetPanel(
-      tabPanel("Plot", plotOutput("speedPlot")), 
-      tabPanel("Summary", verbatimTextOutput("summary")), 
-      tabPanel("Table", tableOutput("table"))
-    )    
-
-    #h3(textOutput("caption"))
+    h3(textOutput("caption1")),
+  
+    h3(textOutput("caption"))
 
     #plotOutput("speedPlot")
     
